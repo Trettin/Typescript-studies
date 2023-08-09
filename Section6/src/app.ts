e69d0de2abb1075b64ff327df6deca5940c15e74
trettin <gtcastro7@gmail.com>
2023-08-09 09:15:21 -0300

    type guard lesson

--- Section6/src/app.ts
+++ Section6/src/app.ts
@@ -24,3 +24,55 @@ type Combinable = string | number;
 type Numeric = number | boolean;
 
 type Universal = Combinable & Numeric;
+
+// Type guard
+function add(a: Combinable, b: Combinable) {
+  if (typeof a === 'string' || typeof b === 'string') {
+    return a.toString() + b.toString();
+  }
+  return a + b;
+}
+
+type UnknownEmployee = Employee | Admin;
+
+// Type guard for properties in custom types that don't exist in run time 
+function printEmployeeInformation(emp: UnknownEmployee) {
+  console.log('Name ' + emp.name);
+  if ('privileges' in emp) {
+    console.log('Privileges ' + emp.privileges);
+  }
+  if ('startDate' in emp) {
+    console.log('Start date ' + emp.startDate);
+  }
+}
+
+printEmployeeInformation(e1);
+
+class Car {
+  drive() {
+    console.log('Driving...');
+  }
+}
+
+class Truck {
+  drive() {
+    console.log('Driving a truck...');
+  }
+
+  loadCargo(amount: number) {
+    console.log('Loading cargo ...' + amount);
+  }
+}
+
+type Vehicle = Car | Truck;
+
+const v1 = new Car();
+const v2 = new Truck();
+
+function useVehicle(vehicle: Vehicle) {
+  vehicle.drive();
+  // Type guard for class that works in runtime
+  if (vehicle instanceof Truck) {
+    vehicle.loadCargo(1000);
+  }
+}
