224538b6eca224da8005c9ded08dccf8e7500a51
trettin <gtcastro7@gmail.com>
2023-09-06 22:19:12 -0300

    modified WithTemplate decorator to return a constructor function, this way it only take effect once we instantiate the class

--- Section-08-Decorators/src/app.ts
+++ Section-08-Decorators/src/app.ts
@@ -1,95 +1,100 @@
 
-// function Logger(logString: string) {
-//   return function(constructor: Function) {
-//     console.log(logString)
-//     console.log(constructor)
-//   }
-// }
-
-// function WithTemplate(template: string, hookId: string) {
-//   return function(constructor: any) {
-//     console.log('Rendering Template')
-//     const hookEl = document.getElementById(hookId);
-
-//     const person = new constructor('Gabss');
-
-//     if (hookEl) {
-//       hookEl.innerHTML = template;
-//       hookEl.querySelector('h2')!.textContent = person.name;
-//     }
-//   }
-// }
-
-// // Decorators are executed when classes are defined, not instantiated
-// @Logger('Test log')
-// @WithTemplate(`
-// <h1>My person Obj</h1>
-// <h2></h2> 
-// `
-// ,'app')
-// class Person {
-
-//   constructor(public name: string) {
-//     console.log('Constructor')
-//   } 
-// }
-
-// const pers = new Person('Gab');
-
-// console.log(pers);
-
-function Log(target: any, propName: string) {
-  console.log('Property name: ', propName);
-  console.log('Property constructor', target);
+function Logger(logString: string) {
+  return function(constructor: Function) {
+    console.log(logString)
+    console.log(constructor)
+  }
 }
 
-function Log2(target: any, name: string, descripor: PropertyDescriptor) {
-  console.log('Acessor decorator!');
-  console.log(target);
-  console.log(name);
-  console.log(descripor);
-}
+function WithTemplate(template: string, hookId: string) {
+  return function<T extends {new(...args: any[]): {name: string}}>(_constructor: T) {
+    console.log('Rendering Template')
+    
+    return class extends _constructor {
+      constructor(...args: any[]) {
+        super(...args);
 
-function Log3(target: any, name: string | Symbol, descripor: PropertyDescriptor) {
-  console.log('Method decorator!');
-  console.log(target);
-  console.log(name);
-  console.log(descripor);
-}
+        const hookEl = document.getElementById(hookId);
 
-/**
- * @param target 
- * @param name Name of the function
- * @param argPosition 
- */
-function Log4(target: any, name: string | Symbol, argPosition: number) {
-  console.log('Parameter decorator!');
-  console.log(target);
-  console.log(name);
-  console.log(argPosition);
-}
-
-class Product {
-  @Log
-  title: string;
-  private _price: number;
-
-  @Log2
-  set price(val: number) {
-    if (val > 0) {
-      this._price = val;
-      return;
+        if (hookEl) {
+          hookEl.innerHTML = template;
+          hookEl.querySelector('h2')!.textContent = this.name;
+        }
+      }
     }
-    throw new Error('Invalid Price - Price should be positive.')
-  }
-
-  constructor(t: string, p: number) {
-    this.title = t;
-    this.price = p;
-  }
-
-  @Log3
-  getPriceWithTax(@Log4 tax: number) {
-    return this._price * (1 + tax);
   }
 }
+
+// Decorators are executed when classes are defined, not instantiated
+@Logger('Test log')
+@WithTemplate(`
+<h1>My person Obj</h1>
+<h2></h2> 
+`
+,'app')
+class Person {
+
+  constructor(public name: string) {
+    console.log('Constructor')
+  } 
+}
+
+const pers = new Person('Gab');
+
+console.log(pers);
+
+// function Log(target: any, propName: string) {
+//   console.log('Property name: ', propName);
+//   console.log('Property constructor', target);
+// }
+
+// function Log2(target: any, name: string, descripor: PropertyDescriptor) {
+//   console.log('Acessor decorator!');
+//   console.log(target);
+//   console.log(name);
+//   console.log(descripor);
+// }
+
+// function Log3(target: any, name: string | Symbol, descripor: PropertyDescriptor) {
+//   console.log('Method decorator!');
+//   console.log(target);
+//   console.log(name);
+//   console.log(descripor);
+// }
+
+// /**
+//  * @param target 
+//  * @param name Name of the function
+//  * @param argPosition 
+//  */
+// function Log4(target: any, name: string | Symbol, argPosition: number) {
+//   console.log('Parameter decorator!');
+//   console.log(target);
+//   console.log(name);
+//   console.log(argPosition);
+// }
+
+// class Product {
+//   @Log
+//   title: string;
+//   private _price: number;
+
+//   @Log2
+//   set price(val: number) {
+//     if (val > 0) {
+//       this._price = val;
+//       return;
+//     }
+//     throw new Error('Invalid Price - Price should be positive.')
+//   }
+
+//   constructor(t: string, p: number) {
+//     this.title = t;
+//     this.price = p;
+//   }
+
+//   @Log3
+//   getPriceWithTax(@Log4 tax: number) {
+//     return this._price * (1 + tax);
+//   }
+// }
