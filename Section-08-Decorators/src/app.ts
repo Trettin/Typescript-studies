04ab07d3eea8968026f87bef0b5feadda29256d2
trettin <gtcastro7@gmail.com>
2023-09-06 21:22:48 -0300

    playing a little with the decorator

--- Section-08-Decorators/src/app.ts
+++ Section-08-Decorators/src/app.ts
@@ -10,25 +10,28 @@ function WithTemplate(template: string, hookId: string) {
   return function(constructor: any) {
     const hookEl = document.getElementById(hookId);
 
-    const person = new constructor();
+    const person = new constructor('Gabss');
 
     if (hookEl) {
       hookEl.innerHTML = template;
-      hookEl.querySelector('h1')!.textContent = person.name;
+      hookEl.querySelector('h2')!.textContent = person.name;
     }
   }
 }
 
 // Decorators are executed when classes are defined, not instantiated
-@WithTemplate('<h1>My person Obj</h1>','app')
+@WithTemplate(`
+<h1>My person Obj</h1>
+<h2></h2> 
+`
+,'app')
 class Person {
-  name = 'Max';
 
-  constructor() {
+  constructor(public name: string) {
     console.log('Constructor')
   } 
 }
 
-const pers = new Person();
+const pers = new Person('Gab');
 
 console.log(pers)
