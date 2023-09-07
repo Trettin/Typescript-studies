a2b63d431c34354fe172bf617d91ef3c45eafbac
trettin <gtcastro7@gmail.com>
2023-09-06 22:41:27 -0300

    created autobind decorator to override propertyDescriptor

--- Section-08-Decorators/src/app.ts
+++ Section-08-Decorators/src/app.ts
@@ -43,58 +43,105 @@ const pers = new Person('Gab');
 
 console.log(pers);
 
-// function Log(target: any, propName: string) {
-//   console.log('Property name: ', propName);
-//   console.log('Property constructor', target);
-// }
+function Log(target: any, propName: string) {
+  console.log('Property name: ', propName);
+  console.log('Property constructor', target);
+}
 
-// function Log2(target: any, name: string, descripor: PropertyDescriptor) {
-//   console.log('Acessor decorator!');
-//   console.log(target);
-//   console.log(name);
-//   console.log(descripor);
-// }
+/**
+ * Acessor and method decorators can return values
+ * We can return a new PropertyDescriptor
+ * configurable: true
+ * enumerable: false
+ * get: undefined
+ * set: ƒ price(val)
+ */
+function Log2(target: any, name: string, descripor: PropertyDescriptor) {
+  console.log('Acessor decorator!');
+  console.log(target);
+  console.log(name);
+  console.log(descripor);
+}
 
-// function Log3(target: any, name: string | Symbol, descripor: PropertyDescriptor) {
-//   console.log('Method decorator!');
-//   console.log(target);
-//   console.log(name);
-//   console.log(descripor);
-// }
+/**
+ * 
+ * @param target 
+ * @param name 
+ * @param descripor {
+ *  configurable: true
+ *  enumerable: false
+ *  value: ƒ getPriceWithTax(tax)
+ *  writable true
+ * } 
+ */
+function Log3(target: any, name: string | Symbol, descripor: PropertyDescriptor) {
+  console.log('Method decorator!');
+  console.log(target);
+  console.log(name);
+  console.log(descripor);
+}
 
-// /**
-//  * @param target 
-//  * @param name Name of the function
-//  * @param argPosition 
-//  */
-// function Log4(target: any, name: string | Symbol, argPosition: number) {
-//   console.log('Parameter decorator!');
-//   console.log(target);
-//   console.log(name);
-//   console.log(argPosition);
-// }
+/**
+ * @param target 
+ * @param name Name of the function
+ * @param argPosition 
+ */
+function Log4(target: any, name: string | Symbol, argPosition: number) {
+  console.log('Parameter decorator!');
+  console.log(target);
+  console.log(name);
+  console.log(argPosition);
+}
 
-// class Product {
-//   @Log
-//   title: string;
-//   private _price: number;
+class Product {
+  @Log
+  title: string;
+  private _price: number;
 
-//   @Log2
-//   set price(val: number) {
-//     if (val > 0) {
-//       this._price = val;
-//       return;
-//     }
-//     throw new Error('Invalid Price - Price should be positive.')
-//   }
+  @Log2
+  set price(val: number) {
+    if (val > 0) {
+      this._price = val;
+      return;
+    }
+    throw new Error('Invalid Price - Price should be positive.')
+  }
 
-//   constructor(t: string, p: number) {
-//     this.title = t;
-//     this.price = p;
-//   }
+  constructor(t: string, p: number) {
+    this.title = t;
+    this.price = p;
+  }
 
-//   @Log3
-//   getPriceWithTax(@Log4 tax: number) {
-//     return this._price * (1 + tax);
-//   }
-// }
+  @Log3
+  getPriceWithTax(@Log4 tax: number) {
+    return this._price * (1 + tax);
+  }
+}
+
+function Autobind(_: any, _2: string | Symbol, descriptor: PropertyDescriptor): PropertyDescriptor {
+  const originalMethod = descriptor.value;
+  const adjDescriptor: PropertyDescriptor = {
+    configurable: true,
+    enumerable: false,
+    get() {
+      const boundFn = originalMethod.bind(this);
+      return boundFn
+    }
+  }
+  return adjDescriptor;
+}
+class Printer {
+  message = 'This works'
+
+  @Autobind
+  showMessage() {
+    console.log(this.message)
+  }
+}
+
+const p = new Printer();
+const button = document.querySelector('button');
+button?.addEventListener('click', p.showMessage);
+
+
+
